45e6a706f93f53cf7fcdd7871941d42882f1df05
Michael-Chen-24 <zheng_xichen@163.com>
2020-09-14 17:09:56 +0800

    Preprocess

--- Dataprocessing.py
+++ Dataprocessing.py
@@ -8,6 +8,7 @@ import math
 import csv
 import numpy as np
 import configparser as ConfigParser
+from sklearn import preprocessing
 import matplotlib.pyplot as plt
 import matplotlib.colors as colors
 
@@ -17,17 +18,16 @@ def read_config():
     config.read('config.cfg', encoding = 'utf-8')
     config_dict = {}
     config_dict['DATA_File'] = config.get('section', 'DATA_File')
-    config_dict['dismision'] = config.getint('section', 'dismision')
     config_dict['k'] = config.getint('section', 'k')
     config_dict['beta'] = config.getfloat('section', 'beta')
     config_dict['ann'] = config.get('section', 'ann')
+    config_dict['metric'] = config.get('section', 'metric')
     config_dict['n_clusters'] = config.getint('section', 'n_clusters')
     return config_dict
-
-
+    
 # 读取数据集，传入文件名和维度，将数据集读取出来并返回
 # 数据集每一行的格式是：X, Y, Z, ..., label
-def get_data(filename, dismision):
+def get_data(filename):
     data = []
     label = []
 
@@ -35,13 +35,18 @@ def get_data(filename, dismision):
         csv_reader = csv.reader(file_obj)
         for row in csv_reader:
             point = []
-            for dim in range(dismision):
-                point.append(float(row[dim]))
+            for d in row[:-1]:
+                point.append(float(d))
             data.append(point)
             # if row[-1] == 0, int(row[-1]) will fail
             label.append(int(float(row[-1])))
 
-    return np.array(data, np.float32), np.array(label, np.int8)
+    X = np.array(data)
+    min_max_scaler = preprocessing.MinMaxScaler() 
+    X_minMax = min_max_scaler.fit_transform(X)
+    return X_minMax, np.array(label, np.int8)
+
+    # return np.array(data, np.float32), np.array(label, np.int8)
 
 
 # 计算欧几里得距离,a,b分别为两个元组
@@ -54,27 +59,6 @@ def dist(a, b):
     return buffer
 
 
-def show_cluster(data, label_pred, center):
-    plt.figure()
-    plt.clf()
-
-    X = []
-    Y = []
-    for point in data:
-        X.append(point[0])
-        Y.append(point[1])
-    plt.scatter(x=X, y=Y, c=label_pred, s=10)
-    
-    top_center_X = []
-    top_center_Y = []
-    for point in center:
-        top_center_X.append(point[0])
-        top_center_Y.append(point[1])
-    plt.scatter(x=top_center_X, y=top_center_Y, c='red', marker='*', s=20)
-
-    plt.show()
-
-
 def maxminnorm(array):
     '''
     normalization function: aim to normalize the rho and delta
@@ -84,88 +68,109 @@ def maxminnorm(array):
     return array
 
 
-def show_diagram(radius, weight, choosen_id):
-    plt.figure()
-    center= [i for i in range(len(radius))]
-    top_K = choosen_id
-    no_top_K = set(center).difference(set(top_K))
+def id_diagram(c_original_center, cc_mode_set, cc_set, rho_original, delta):
+    '''
+    c_original_center : pick center，选中的K个中心点
+    cc_mode_set : the first level cc mode set, （data -- find_cc -- cc set）
+                    在第二层中，所有的CC的密度最高点（下一层的输入点）
+    cc_set: cc point set,在第二层中，cc中所有的点的集合
 
-    rho = 1./radius
-    rho = maxminnorm(rho)
-    # weight = maxminnorm(weight)
+    '''
+    rho_original = maxminnorm(rho_original)
+    delta_original = [0. for i in range(len(rho_original))]
+    
+    for i, d in zip(cc_mode_set, delta):
+        delta_original[i] = d
+    
+    plt.figure(figsize=[6.40,5.60])
+    point_set  = [i for i in range(len(rho_original))]
+    point_set = list(set(point_set).difference(cc_set))
+    cc_set = list(set(cc_set).difference(set(cc_mode_set)))
+    cc_mode_set = list(set(cc_mode_set).difference(set(c_original_center)))
 
-    top_K_X = []
-    top_K_Y = []
-    for i in top_K:
-        top_K_X.append(rho[i])
-        top_K_Y.append(weight[i])
-    plt.scatter(x=top_K_X, y=top_K_Y, c='red', marker='*', s=30)
 
-    no_top_K_X = []
-    no_top_K_Y = []
-    for i in no_top_K:
-        no_top_K_X.append(rho[i])
-        no_top_K_Y.append(weight[i])
-    plt.scatter(x=no_top_K_X, y=no_top_K_Y, c='blue', marker='X', s=20)
+    mode_X = []
+    mode_Y = []
+    for m in cc_mode_set:
+        mode_X.append(rho_original[m])
+        mode_Y.append(delta_original[m])
+    plt.scatter(mode_X, mode_Y, marker='.', s=200, c='blue', label='centers of other DSs')
+
+
+    center_X = []
+    center_Y = []
+    for c in c_original_center:
+        center_X.append(rho_original[c])
+        center_Y.append(delta_original[c])
+    plt.scatter(center_X, center_Y, marker='*', s=200, label='centers of top-K DSs', c='red')
+    
+    plt.title('Importance Diagram',fontsize=30)
+    plt.xlabel('Density:1/$r_k(x_i)$',fontsize=15)
+    plt.ylabel('Geometric Weight:$w_i$',fontsize=15)
+    ax=plt.gca()
+    handles, labels = ax.get_legend_handles_labels()
+    ax.legend(handles[::-1], labels[::-1], ncol=2, loc='lower center', bbox_to_anchor=(0.5,1), fontsize=12, frameon=False)
     plt.show()
 
 
-def show_center(choosen_id, DS_index, data):
-    top_center = []
-    for i in choosen_id:
-        top_center.append(DS_index[i])
-    data_index = [i for i in range(len(data))]
-    no_center = set(data_index).difference(set(top_center))
+def plot_quickdsc(data, c_original_center, cc_mode_set, cc_set):
+    plt.figure(figsize=[6.40,5.60])
 
-    X = []
-    Y = []
-    for n in no_center:
-        X.append(data[n][0])
-        Y.append(data[n][1])
-    plt.scatter(x=X, y=Y, c='black', s=3)
-   
-    top_center_X = []
-    top_center_Y = []
-    for i in top_center:
-        top_center_X.append(data[i][0])
-        top_center_Y.append(data[i][1])
-    plt.scatter(x=top_center_X, y=top_center_Y, c='red', marker='*', s=20)
+    point_set  = [i for i in range(len(data))]
+    point_set = list(set(point_set).difference(cc_set))
+    cc_set = list(set(cc_set).difference(set(cc_mode_set)))
+    cc_mode_set = list(set(cc_mode_set).difference(set(c_original_center)))
 
+    point_X = []
+    point_Y = []
+    for p in point_set:
+        point_X.append(data[p][0])
+        point_Y.append(data[p][1])
+    plt.scatter(point_X, point_Y, marker='x', c='grey', s=50, label='non-DS samples')
+
+
+    cc_X = []
+    cc_Y = []
+    for c in cc_set:
+        cc_X.append(data[c][0])
+        cc_Y.append(data[c][1])
+    plt.scatter(cc_X,cc_Y, marker='.', c='black', s=100, label='non-center samples in DSs')
+
+
+    mode_X = []
+    mode_Y = []
+    for m in cc_mode_set:
+        mode_X.append(data[m][0])
+        mode_Y.append(data[m][1])
+    plt.scatter(mode_X, mode_Y, marker='.', s=160, c='blue', label='centers of other DSs')
+
+
+    center_X = []
+    center_Y = []
+    for c in c_original_center:
+        center_X.append(data[c][0])
+        center_Y.append(data[c][1])
+    plt.scatter(center_X, center_Y, marker='*', s=200, label='centers of top-K DSs', c='red')
+    
+    
+    ax=plt.gca()
+    handles, labels = ax.get_legend_handles_labels()
+    # reverse the order
+    ax=plt.gca()
+    ax.legend(handles[::-1], labels[::-1], ncol=2, loc='lower center', bbox_to_anchor=(0.5,1), fontsize=12, frameon=False)
     plt.show()
 
-def show_data(data):
+
+def show_cluster(data, label_pred):
+    # plt.figure()
+    plt.figure(figsize=[6.40,5.60])
+
     X = []
     Y = []
     for point in data:
         X.append(point[0])
         Y.append(point[1])
-    plt.scatter(x=X, y=Y, c='black', s=3)
-    plt.show()
-
-def show_cluster_data(DS_data, label_pred, center, data):
-    plt.figure()
-    plt.clf()
-
-    background_X = []
-    background_Y = []
-    for point in data:
-        background_X.append(point[0])
-        background_Y.append(point[1])
-    plt.scatter(x=background_X, y=background_Y, c='red', s=5)
-    
-    X = []
-    Y = []
-    for point in DS_data:
-        X.append(point[0])
-        Y.append(point[1])
-    plt.scatter(x=X, y=Y, c=label_pred, s=10)
-    
-    top_center_X = []
-    top_center_Y = []
-    for point in center:
-        top_center_X.append(point[0])
-        top_center_Y.append(point[1])
-    plt.scatter(x=top_center_X, y=top_center_Y, c='red', marker='*', s=20)
-
-   
+    # plt.scatter(x=X, y=Y, c=label_pred, cmap='Set1', s=8)
+    plt.scatter(x=X, y=Y, c=label_pred, s=8)
+    # plt.scatter(x=X, y=Y, c=label_pred, s=8)
     plt.show()
