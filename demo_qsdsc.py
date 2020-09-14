bd1cf2e9482a2d88cd9143707ffd0ead1a6b585b
Michael-Chen-24 <zheng_xichen@163.com>
2020-09-14 16:53:01 +0800

    Demo

--- demo_qsdsc.py
+++ demo_qsdsc.py
@@ -4,7 +4,7 @@
 # author "chen"
 # Time 2020/5/20
 
-# run the quickshiftPP, attain the density subgraph
+# run the find_subgraph, attain the density subgraph
 
 import datetime
 import Dataprocessing
@@ -19,30 +19,31 @@ if __name__ == '__main__':
     
     # parameter
     DATA_File = config_dict['DATA_File']
-    dismision = config_dict['dismision']
+    
     k = config_dict['k']
     n_clusters = config_dict['n_clusters']
     beta = config_dict['beta']
     ann = config_dict['ann']
+    metric = config_dict['metric']
 
     # 数据集和标签
-    data, label_true = Dataprocessing.get_data(DATA_File, dismision)
+    data, label_true = Dataprocessing.get_data(DATA_File)
     print(data.shape)
 
-    # starttime = datetime.datetime.now()
-    model = QuickDSC(k, n_clusters, beta, ann=ann)
+    starttime = datetime.datetime.now()
+
+    model = QuickDSC(k, n_clusters, beta, ann=ann, metric=metric)
     model.fit(data)
-    # endtime = datetime.datetime.now()
-    # print('total time=', endtime-starttime)
     label_pred = model.labels_
-    print('quickshift remain ARI:', metrics.adjusted_rand_score(label_true, label_pred))
-    print('quickshift remain AMI:', metrics.adjusted_mutual_info_score(label_true, label_pred))
+    
+    endtime = datetime.datetime.now()
+    print('total time=', endtime-starttime)
 
-    # Dataprocessing.show_cluster(data, label_pred)
-    # radius = model.rho_radius
-    # weight = model.delta
-    # choosen_id = model.choosen_id
-    # Dataprocessing.show_diagram(radius, weight, choosen_id)
+    ARI=metrics.adjusted_rand_score(label_true, label_pred)
+    AMI=metrics.adjusted_mutual_info_score(label_true, label_pred)
+    NMI=metrics.normalized_mutual_info_score(label_true, label_pred)
 
-    # DS_index = model.DS_index
-    # Dataprocessing.show_center(choosen_id, DS_index, data)
+    print("Adj. Rand Index Score=" , ARI)
+    print("Adj. Mutual Info Score=", AMI)
+    print("Norm Mutual Info Score=", NMI)
+    
